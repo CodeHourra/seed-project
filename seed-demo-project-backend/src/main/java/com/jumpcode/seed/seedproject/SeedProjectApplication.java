3dd78130b4c297b4c2a12c3133d4caf9e5a05f06
liugan <925060392@qq.com>
2020-05-03 23:13:32 +0800

    chore: 初始化种子工程

---
+++ seed-demo-project-backend/src/main/java/com/jumpcode/seed/seedproject/SeedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jumpcode.seed.seedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedProjectApplication {
+		
+		public static void main(String[] args) {
+				SpringApplication.run(SeedProjectApplication.class, args);
+		}
+		
+}
